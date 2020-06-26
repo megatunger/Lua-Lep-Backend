504d64a91e304305b3b2b6455677bccb0452d3e3
Hoang Son Tung <megatunger@gmail.com>
2020-06-26 17:10:01 +0700

    updating word api

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ import json
 from flask import Flask, request
 from decouple import config
 import requests
+
 API_KEY = config('GOOGLE_STT_API_KEY')
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def welcome():
     return 'Welcome to Lua Lep API!'
+
 
 @app.route('/api/sentence/<file_name>', methods=['POST'])
 def callSpeechToTextAPI(file_name):
@@ -22,11 +25,13 @@ def callSpeechToTextAPI(file_name):
             "sampleRateHertz": 44100
         },
         "audio": {
-            "uri": "gs://lua-lep-app.appspot.com/recordings/"+file_name
+            "uri": "gs://lua-lep-app.appspot.com/recordings/" + file_name
         }
     }
-    response = requests.post('https://speech.googleapis.com/v1/speech:recognize?key='+API_KEY, headers=headers, data=json.dumps(data))
+    response = requests.post('https://speech.googleapis.com/v1/speech:recognize?key=' + API_KEY, headers=headers,
+                             data=json.dumps(data))
     return response.content, headers
+
 
 @app.route('/api/word', methods=['POST'])
 def checkWord():
@@ -34,14 +39,40 @@ def checkWord():
     message = ""
     word = request.args.get('word', None)
     audio_file = request.args.get('audio_file', None)
-
+    headers = {
+        'Content-type': 'application/json',
+    }
+    data = {
+        "config": {
+            "encoding": "LINEAR16",
+            "languageCode": "vi-VN",
+            "sampleRateHertz": 44100
+        },
+        "audio": {
+            "uri": "gs://lua-lep-app.appspot.com/recordings/" + audio_file
+        }
+    }
+    response = requests.post('https://speech.googleapis.com/v1/speech:recognize?key=' + API_KEY, headers=headers,
+                             data=json.dumps(data))
+    confidence = 0.0
+    accuracy = ''
+    try:
+        word_recognize = response.json().get('results')[0].get('alternatives')[0].get('transcript')
+        confidence = response.json().get('results')[0].get('alternatives')[0].get('confidence')
+        if str(word_recognize) != word or confidence < 0.7:
+            passed = False
+    except:
+        passed = False
     if passed:
+        accuracy = "Độ chính xác "+"{:.2f}".format(confidence * 100) + "%"
         message = "Bạn đã đọc đúng ✅"
     else:
+        accuracy = "Có vẻ như bạn bị ngọng"
         message = "Mời bạn đọc lại ❗️"
+
     return {
         "word": word,
-        "accuracy": "100%",
+        "accuracy": accuracy,
         "passed": passed,
         "message": message
     }
